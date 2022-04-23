d6f49ed6945ccb47a3c50c3b553ab490c0afd028
pradekabrilyan <pradekabrilyan@gmail.com>
2022-04-23 10:58:43 +0700

    tambah koding kedua pada dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,5 +9,7 @@ class DashboardController extends Controller
     function index() 
     {
     	echo "Saya Halaman Dashboard";
+    	echo "<h1> Ini adalah koding kedua pada Dashboard </h1>";
+    	echo "<p> harapannya ini akan tercatat pada koding kedua saya </p>";
     }
 }
